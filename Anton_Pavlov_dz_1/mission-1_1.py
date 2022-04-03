a34462f82325f7b62e2448365a34b56d7a467122
Skvall <antonalizee@yandex.ru>
2022-04-03 10:06:46 +0300

    Изменил версию первой задачи, работа с отрицательными значениями.

--- Anton_Pavlov_dz_1/mission-1_1.py
+++ Anton_Pavlov_dz_1/mission-1_1.py
@@ -1,6 +1,10 @@
 duration = int(input('Введите число в секундах: '))
-sec = duration % 60
-minute = duration // 60 % 60
-hour = duration // 60 // 60 % 24
-days = duration // 60 // 60 // 24 % 30
-print(f'{days} день {hour} час {minute} мин {sec} сек')
+if duration < 0:
+    print('Вводите положительное значение')
+    duration = int(input('Введите число в секундах: '))
+else:
+    sec = duration % 60
+    minute = duration // 60 % 60
+    hour = duration // 60 // 60 % 24
+    days = duration // 60 // 60 // 24 % 30
+    print(f'{days} день {hour} час {minute} мин {sec} сек')
