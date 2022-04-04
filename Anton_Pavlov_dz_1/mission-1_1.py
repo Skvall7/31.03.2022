555ad71543ebbcf3196f0e7b85cbf33f147de648
Skvall <antonalizee@yandex.ru>
2022-04-04 19:22:55 +0300

    В первом задании: Исключил завершение программы до ввода нужного значения, изменил вывод без лишних значений. Во втором задании: Неправильно была понята задача, исправлено.

--- Anton_Pavlov_dz_1/mission-1_1.py
+++ Anton_Pavlov_dz_1/mission-1_1.py
@@ -1,5 +1,5 @@
 duration = int(input('Введите число в секундах: '))
-if duration < 0:
+while duration < 0:
     print('Вводите положительное значение')
     duration = int(input('Введите число в секундах: '))
 else:
@@ -7,4 +7,11 @@ else:
     minute = duration // 60 % 60
     hour = duration // 60 // 60 % 24
     days = duration // 60 // 60 // 24 % 30
+if duration < 60:
+    print(f'{sec} сек')
+elif 60 <= duration < 3600:
+    print(f'{minute} мин {sec} сек')
+elif 3600 <= duration < 86400:
+    print(f'{hour} час {minute} мин {sec} сек')
+else:
     print(f'{days} день {hour} час {minute} мин {sec} сек')
--- Anton_Pavlov_dz_1/mission-1_2.py
+++ Anton_Pavlov_dz_1/mission-1_2.py
@@ -1,15 +1,17 @@
-list_cub = [149 ** 3, 18 ** 3, 998 ** 3]
-print(list_cub)
+list_cub = []
+index = 1
+result = 0
+result_2 = 0
+while index < 1000:
+    list_cub.append(index ** 3)
+    index += 2
 for number in list_cub:
     calculation = number % 10000000000 // 1000000000 + number % 1000000000 // 100000000 + number % 100000000 // 10000000 + number % 10000000 // 1000000 + number % 1000000 // 100000 + number % 100000 // 10000 + number % 10000 // 1000 + number % 1000 // 100 + number % 10 + number % 100 // 10
     if calculation % 7 == 0:
-        text = 'целое число'
-    else:
-        text = 'не целое число'
-    print(f'{calculation} делённое на 7 {text}')
-    if (calculation + 17) % 7 == 0:
-        text = 'целое число'
-    else:
-        text = 'не целое число'
-    print(f'{calculation} + 17 делённое на 7 {text}')
-
+        result = result + number
+    number_2 = number + 17
+    calculation_2 = number_2 % 10000000000 // 1000000000 + number_2 % 1000000000 // 100000000 + number_2 % 100000000 // 10000000 + number_2 % 10000000 // 1000000 + number_2 % 1000000 // 100000 + number_2 % 100000 // 10000 + number_2 % 10000 // 1000 + number_2 % 1000 // 100 + number_2 % 10 + number_2 % 100 // 10
+    if calculation_2 % 7 == 0:
+        result_2 = result_2 + number_2
+print(f'Результат пункта A: {result}')
+print(f'Результат пункта B: {result_2}')
